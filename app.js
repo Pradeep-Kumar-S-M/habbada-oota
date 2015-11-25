110af5d76599e4b5cd8a9e6f59dc68103f0ed30f
Pradeep Kumar Siddappa Malleshappa <pradeep.ksm@flipkart.com>
2015-11-25 11:44:04 +0530

    Testing

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
 var express = require('express');
 var app = express();
 
-var browserify = require('browserify');
-var path = require('path');
+app.set('port', (process.env.PORT || 5000));
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+// var express = require('express');
+// var app = express();
+
+// var browserify = require('browserify');
+// var path = require('path');
+
+// // viewed at http://localhost:8080
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
 
     
-});
+// });
 
 
 
-app.all('*',function(req,res){
-	console.log("Req");
+// app.all('*',function(req,res){
+// 	console.log("Req");
 
-	 if (req.url === '/bundle.js') {
-	 	console.log('inside bundele');
-        res.setHeader('content-type', 'application/javascript');
-        var b = browserify(__dirname + '/public/angularDemo.js').bundle();
-        b.on('error', console.error);
-        b.pipe(res);
-    }
-    else res.writeHead(404, 'not found')
+// 	 if (req.url === '/bundle.js') {
+// 	 	console.log('inside bundele');
+//         res.setHeader('content-type', 'application/javascript');
+//         var b = browserify(__dirname + '/public/angularDemo.js').bundle();
+//         b.on('error', console.error);
+//         b.pipe(res);
+//     }
+//     else res.writeHead(404, 'not found')
 
-});
+// });
 
 
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-var server = app.listen(3000, function(){
-	var host = server.address().address;
-	var port = server.address().port;
+// var server = app.listen(3000, function(){
+// 	var host = server.address().address;
+// 	var port = server.address().port;
 
-	console.log(host + " port - " + port);
-});
+// 	console.log(host + " port - " + port);
+// });
+
