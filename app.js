50b00c6e00dca6c1ed0230750c30ebb8d76d11df
Pradeep Kumar Siddappa Malleshappa <pradeep.ksm@flipkart.com>
2015-11-25 12:16:55 +0530

    Update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var browserify = require('browserify');
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,22 +15,23 @@ app.use(express.static(__dirname + '/public'));
 //   response.render('pages/index');
 // });
 
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+
 
 
 // var express = require('express');
 // var app = express();
 
-// var browserify = require('browserify');
-// var path = require('path');
 
-// viewed at http://localhost:8080
+
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 
     
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
